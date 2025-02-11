659c775868f84df7badb798062bead561396a4a1
Gayrat Rahmatulayev <gayratrahmatulayev8@gmail.com>
2025-02-11 20:00:12 +0300

    Created an object turtle(player) and features

--- main.py
+++ main.py
@@ -5,10 +5,18 @@ from car_manager import CarManager
 from scoreboard import Scoreboard
 
 screen = Screen()
+screen.listen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+
+turtle = Player()
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    screen.onkey(key="Up", fun=turtle.move_up)
+
+    # Detect when turtle(player) reach the finish.
+    if turtle.ycor() >= 280:
+        turtle.reset_finish()
--- player.py
+++ player.py
@@ -1,7 +1,19 @@
-STARTING_POSITION = (0, -280)
+from turtle import Turtle
+
 MOVE_DISTANCE = 10
-FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.color("black")
+        self.penup()
+        self.setheading(90)
+        self.reset_finish()
+
+    def move_up(self):
+        self.forward(MOVE_DISTANCE)
+
+    def reset_finish(self):
+        self.goto(0, -280)
